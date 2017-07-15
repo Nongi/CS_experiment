1ee64db100968e23d098a367289536f6ea1328db
Nongi <quentin.zim@gmail.com>
2017-07-15 02:54:15 +0200

    Ajouts des premiers éléments
    - ajout du répertoire des sources
    - ajout du fichier exemple xml décrivant des tables
    - ajout de la classe C# pour gérer les tables de données
    - ajout de la fonction de conversion XML -> SQL (vertica)

---
+++ CS_experiment/Sources/tableHandler.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace CS_experiment.Sources
+{
+    public class tableHandler
+    {
+        public static string xml2sql(string xml_path)
+        {
+            using (XmlReader reader = XmlReader.Create(xml_path))
+            {
+                // Parse the file and display each of the nodes.
+                while (reader.Read())
+                {
+                    Console.WriteLine(reader.Value);
+                }
+            }
+            return "";
+        }
+    }
+}
