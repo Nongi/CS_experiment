d1623704bd5141f798dc9ab7d960456c7b55d600
Nongi <quentin.zim@gmail.com>
2017-07-17 02:12:04 +0200

    Lot 2 - DDL DB2 to xml
    v0.1 DDL DB2 to xml file
    - add a Table_Structure builder using DDL DB2 description as input (need
    to fill input file type for future upgrade and schema)
    - add methods to save a list of Table_Structure in a xml file

--- CS_experiment/Program.cs
+++ CS_experiment/Program.cs
@@ -12,10 +12,14 @@ namespace CS_experiment
         {
             Console.WriteLine(DateTime.Now + " - Start");
 
-            Sources.tableHandler.xml_to_sql(
-                    @"C:\Users\Nongi\Documents\GitHub\CS_experiment\CS_experiment\Sources\tables_exemple.xml",
-                    @"C:\Users\Nongi\Desktop\sql_query.sql");
+            List<Sources.Table_Structure> l_tbl = new List<Sources.Table_Structure>();
+            l_tbl.Add(new Sources.Table_Structure(0, @"G:\SD.ZSSIAD.TAZCACC.DDL", "TMPDAY"));
 
+            Sources.tableHandler.struct_to_xml(l_tbl, @"C:\Users\Nongi\Documents\GitHub\CS_experiment\CS_experiment\Sources\tables_exemple_bis.xml");
+
+            Sources.tableHandler.xml_to_sql(
+                    @"C:\Users\Nongi\Documents\GitHub\CS_experiment\CS_experiment\Sources\tables_exemple_bis.xml",
+                    @"C:\Users\Nongi\Desktop\sql_query.sql");
             Console.WriteLine(DateTime.Now + " - End");
 
             //Console.ReadLine();
