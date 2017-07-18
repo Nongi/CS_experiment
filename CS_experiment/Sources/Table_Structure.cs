06d8032adc10a248a04bcc47f0c779723be6127e
Nongi <quentin.zim@gmail.com>
2017-07-19 00:19:53 +0200

    Lot 2 - correctif

--- CS_experiment/Sources/Table_Structure.cs
+++ CS_experiment/Sources/Table_Structure.cs
@@ -42,5 +42,103 @@ namespace CS_experiment.Sources
             keys = new List<string>();
 
         }
+
+        public Table_Structure(int input_type, string path_file, string schema_in)
+        {
+            name = "";
+            schema = schema_in;
+            structure = new List<Tuple<string, string, string>>();
+            keys = new List<string>();
+
+            var ddl_tmp = get_DDL_string(path_file);
+            var lines = get_DDL_lines(ddl_tmp);
+
+            for (int i = 0; i < lines.Count(); i++)
+            {
+                String[] line_ = lines[i];
+
+                if (line_.Count() > 0)
+                {
+                    switch (line_[1])
+                    {
+                        case "TABLE":
+                            name = line_[2].Substring(line_[2].IndexOf('.') + 1);
+                            break;
+                        case "CHAR":
+                            structure.Add(new Tuple<String, String, String>(line_[0], "char", line_[2]));
+                            break;
+                        case "SMALLINT":
+                            structure.Add(new Tuple<String, String, String>(line_[0], "smallint", "5"));
+                            break;
+                        case "INTEGER":
+                            structure.Add(new Tuple<String, String, String>(line_[0], "integer", "10"));
+                            break;
+                        case "DECIMAL":
+                            structure.Add(new Tuple<String, String, String>(line_[0], "decimal", line_[2] + line_[3]));
+                            break;
+                        case "DATE":
+                            structure.Add(new Tuple<String, String, String>(line_[0], "date", "10"));
+                            break;
+                        case "TIME":
+                            structure.Add(new Tuple<String, String, String>(line_[0], "time", "8"));
+                            break;
+                        case "TIMESTAMP":
+                            if (line_[2] == "EXTERNAL")
+                                structure.Add(new Tuple<String, String, String>(line_[0], "timestamp", "19"));
+                            else
+                                structure.Add(new Tuple<String, String, String>(line_[0], "timestamp", "26"));
+                            break;
+                        case "KEY":
+                            /*
+                            if (line_.Length > 2)
+                                primary_key.Add(line_[2]);
+
+                            while (lines[i + 1].Count() == 1) {
+                                i++;
+                                //primary_key += lines[i][0];
+                            }*/
+
+                            break;
+                        default:
+                            break;
+                    }
+                }
+            }
+        }
+
+        public List<String[]> get_DDL_lines(string ddl_tmp)
+        {
+            List<String[]> lines = new List<string[]>();
+
+            foreach (string ddl_line in ddl_tmp.Split(new[] { '\n' }, StringSplitOptions.RemoveEmptyEntries))
+            {
+                string[] line_values = ddl_line.Split(new[] { ' ', '(', ')' }, StringSplitOptions.RemoveEmptyEntries);
+                if (line_values.Length != 0)
+                    lines.Add(line_values);
+            }
+
+            lines.RemoveAt(lines.Count() - 1);
+
+            return lines;
+        }
+
+        public string get_DDL_string(string path_file)
+        {
+            string line;
+            String ddl_tmp = "";
+
+            System.IO.StreamReader file = new System.IO.StreamReader(path_file, Encoding.GetEncoding("iso-8859-1"));
+            while ((line = file.ReadLine()) != null)
+            {
+                ddl_tmp += line + "\n";
+            }
+            file.Close();
+
+            ddl_tmp = ddl_tmp.Substring(ddl_tmp.IndexOf("CREATE TABLE "));
+            ddl_tmp = ddl_tmp.Remove(ddl_tmp.IndexOf("COMMENT"));
+
+            return ddl_tmp;
+        }
+
     }
 }
--- CS_experiment/Sources/tableHandler.cs
+++ CS_experiment/Sources/tableHandler.cs
@@ -15,7 +15,6 @@ namespace CS_experiment.Sources
         /// </summary>
         /// <param name="xml_path">xml which describe the data</param>
         /// <param name="sql_path">sql file that will contain create SQL queries</param>
-        /// <returns></returns>
         public static void xml_to_sql(string xml_path, string sql_path)
         {
             //Clean the output file
@@ -47,13 +46,11 @@ namespace CS_experiment.Sources
                                         xmlReader["type"], 
                                         xmlReader["length"]));
                                 }
-
-
                             }
 
-                            var sql_create = struct_to_tmp_sql(tbl_struct_tmp);
+                            var sql_create = get_vertica_ddl_varchar(tbl_struct_tmp);
+                            //var sql_create = get_vertica_ddl_varchar(tbl_struct_tmp);
                             append_sql_to_file(sql_path, sql_create);
-
                         }
                         catch (Exception err)
                         {
@@ -66,15 +63,75 @@ namespace CS_experiment.Sources
         }
 
         /// <summary>
+        /// Methods to convert a list of Table_Structure which describe data to sql queries 
+        /// </summary>
+        /// <param name="list_table_structure">list which contains all the Table_Structure</param>
+        /// <param name="sql_path">sql file that will contain create SQL queries</param>
+        public static void struct_to_sql(List<Table_Structure> list_table_structure, string sql_path)
+        {
+            //Clean the output file
+            System.IO.File.WriteAllText(sql_path, string.Empty);
+            try
+            {
+                foreach(Table_Structure tbl_struct_tmp in list_table_structure)
+                {
+                    var sql_create = get_vertica_ddl_varchar(tbl_struct_tmp);
+                    //var sql_create = get_vertica_ddl_varchar(tbl_struct_tmp);
+                    append_sql_to_file(sql_path, sql_create);
+                }
+            }
+            catch (Exception err)
+            {
+                //TODO : gérer un fichier de rejet des tables "out"
+                Console.WriteLine(err);
+            }
+        }
+
+        /// <summary>
+        /// Methods to convert a list of Table_Structure which describe data to xml file
+        /// </summary>
+        /// <param name="list_table_structure">list which contains all the Table_Structure</param>
+        /// <param name="xml_path">xml file that will contain data structure</param>
+        public static void struct_to_xml(List<Table_Structure> list_table_structure, string xml_path)
+        {
+            //Clean the output file
+            System.IO.File.WriteAllText(xml_path, string.Empty);
+
+            using (XmlWriter writer = XmlWriter.Create(xml_path))
+            {
+                writer.WriteStartDocument();
+
+                ///
+                writer.WriteStartElement("tables");
+
+                foreach (Table_Structure tbl_struct_tmp in list_table_structure)
+                {
+                    writer.WriteStartElement("table");
+                    writer.WriteAttributeString("name", tbl_struct_tmp.name);
+                    writer.WriteAttributeString("schema", tbl_struct_tmp.schema);
+
+                    foreach (Tuple<string, string, string> field in tbl_struct_tmp.structure)
+                    {
+                        writer.WriteStartElement("field");
+                        writer.WriteAttributeString("name", field.Item1);
+                        writer.WriteAttributeString("type", field.Item2);
+                        writer.WriteAttributeString("length", field.Item3);
+                        writer.WriteEndElement();
+                    }
+                }
+            }
+        }
+
+        /// <summary>
         /// Use the class Table_Structure to create the create query on SQL
-        /// WARNING all fields are cast on VARCHAR for make easy load
+        /// WARNING all fields are cast on VARCHAR to make load easier (If not wanted, please see get_vertica_ddl(Table_Structure tbl_struct))
         /// WARNING my DB target is Vertica, the length are on octet and UTF-8 so I multiply by 4 to take into account all the characters
         /// </summary>
         /// <param name="tbl_struct">Table's structure with table's name, table's schema and all fields</param>
         /// <returns>The create query on SQL for the input structure</returns>
-        public static string struct_to_tmp_sql(Table_Structure tbl_struct)
+        public static string get_vertica_ddl_varchar(Table_Structure tbl_struct)
         {
-            string ddl_TMP = "CREATE TABLE " + tbl_struct.name + " (";
+            string ddl_tmp = "CREATE TABLE " + tbl_struct.schema + "." + tbl_struct.name + " (";
 
             List<string> list_field_tmp = new List<string>();
 
@@ -96,12 +153,64 @@ namespace CS_experiment.Sources
                         list_field_tmp.Add(t.Item1 + " VARCHAR(" + t.Item3 + ")");
                         break;
                 }
+            }
+
+            ddl_tmp += "\r\n\t" + String.Join(",\r\n\t", list_field_tmp) + "\r\n);\n";
+
+            return ddl_tmp;
+        }
+
+        /// <summary>
+        /// Use the class Table_Structure to create the create query on SQL
+        /// WARNING my DB target is Vertica, the length are on octet and UTF-8 so I multiply by 4 to take into account all the characters
+        /// </summary>
+        /// <param name="tbl_struct">Table's structure with table's name, table's schema and all fields</param>
+        /// <returns>The create query on SQL for the input structure</returns>
+        public static string get_vertica_ddl(Table_Structure tbl_struct)
+        {
+            List<string> list_field_tmp = new List<string>();
+            foreach (Tuple<string, string, string> t in tbl_struct.structure)
+            {
+                //TODO : check vertica type
+                switch (t.Item2)
+                {
+                    case "smallint":
+                        list_field_tmp.Add(t.Item1 + " smallint");
+                        break;
+                    case "integer":
+                        list_field_tmp.Add(t.Item1 + " INT");
+                        break;
+                    case "date":
+                        list_field_tmp.Add(t.Item1 + " DATE");
+                        break;
+                    case "time":
+                        list_field_tmp.Add(t.Item1 + " TIME");
+                        break;
+                    case "timestamp":
+                        list_field_tmp.Add(t.Item1 + " TIMESTAMP");
+                        break;
+                    case "char":
+                        list_field_tmp.Add(t.Item1 + " VARCHAR(" + Int32.Parse(t.Item3) * 4 + ")");
+                        break;
+                    case "varchar":
+                        list_field_tmp.Add(t.Item1 + " VARCHAR(" + Int32.Parse(t.Item3) * 4 + ")");
+                        break;
+                    default:
+                        list_field_tmp.Add(t.Item1 + ' ' + t.Item2 + '(' + t.Item3 + ")");
+                        break;
+                }
 
             }
 
-            ddl_TMP += "\r\n\t" + String.Join(",\r\n\t", list_field_tmp);
+            string ddl_tmp = "CREATE TABLE " + tbl_struct.schema + "." + tbl_struct.name + " (";
+            ddl_tmp += "\r\n\t" + String.Join(",\r\n\t", list_field_tmp);
 
-            return ddl_TMP + "\r\n);\n";
+            if (tbl_struct.keys.Count > 0)
+            {
+                ddl_tmp += "\r\n\tPRIMARY KEY (" + String.Join(",\r\n\t\t", tbl_struct.keys) + ")";
+            }
+
+            return ddl_tmp + "\r\n);\n";
         }
 
         /// <summary>
